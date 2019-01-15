69d2a6baf2ebdd796a18e83d391cf21d520789a0
Chris Heuberger <chrisheuberg@gmail.com>
2019-01-15 15:29:54 -0500

    homepage template

--- footer.php
+++ footer.php
@@ -28,7 +28,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 					<div class="site-info">
 
-						<?php understrap_site_info(); ?>
+						<p>&copy; <?php echo date('Y'); ?> Jillian Russo</p>
 
 					</div><!-- .site-info -->
 
